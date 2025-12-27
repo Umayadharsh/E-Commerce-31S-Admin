b6687559f98e8c5435d33ec485a88fc1e6cd69ff
Umaya <umayadharsh@gmail.com>
2025-12-27 12:58:36 +0530

    admin logo fix

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,4 +1,4 @@
-import Logo1 from '../assets/logo1.png'
+import Logo1 from '../assets/Logo1.png'
 import add from '../assets/add.png'
 import order from '../assets/order.png'
 import list from '../assets/list.png'
